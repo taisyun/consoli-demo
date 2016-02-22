7b31551fd8f8c6b5fd9856b00116a088dba3d1a4
Taishun Nakatani <nakatani@diva.co.jp>
2016-02-22 10:56:26 +0900

    Avoid using bootstrap.js and therefore JQuery

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -3,8 +3,8 @@ import { render } from 'react-dom'
 import Root from './containers/Root'
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'expose?$!expose?jQuery!jquery'; // using expose-loader plugin due to bootstrap requires jQuery to be global variable.
-import 'bootstrap/dist/js/bootstrap.js';
+//import 'expose?$!expose?jQuery!jquery'; // using expose-loader plugin due to bootstrap requires jQuery to be global variable.
+//import 'bootstrap/dist/js/bootstrap.js';
 
 import 'react-bootstrap-table/css/react-bootstrap-table-all.css';
 
