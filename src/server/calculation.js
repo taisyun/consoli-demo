1d31a35a6f9466ea334564d3be09b1db3ce17bc0
Taishun Nakatani <nakatani@diva.co.jp>
2016-02-25 11:55:16 +0900

    Refined calculation logic

--- src/server/calculation.js
+++ src/server/calculation.js
@@ -7,33 +7,40 @@ function recalculate(fnclstmt1_store, fnclstmt2_store, consolidated_store) {
   const fnclstmt1_items = (fnclstmt1_store.getState().recordList || {}).items || []
   const fnclstmt2_items = (fnclstmt2_store.getState().recordList || {}).items || []
 
-  const result_items = fnclstmt1_items.map( (item) => {
-    return Object.assign({} , item)
-  })
+  console.log(`fnclstmt1_items.length=${fnclstmt1_items.length} fnclstmt2_items.length=${fnclstmt2_items.length}`)
+  const result_items = []
+  const items_array = [fnclstmt1_items, fnclstmt2_items]
 
-  fnclstmt2_items.forEach( (fnclstmt2_item) => {
-    let index = result_items.findIndex( (result_item) => {
-      return result_item[key] != null && fnclstmt2_item[key] === result_item[key]
-    })
+  items_array.forEach( (items) => {
+    items.forEach( (in_item) => {
 
-    if(index === -1) {
-      result_items.push(fnclstmt2_item)
-    } else {
-      const result_item = result_items[index]
-
-      let value_fnclstmt2 = fnclstmt2_item[value]
-      let value_result = result_item[value]
-
-      if (value_fnclstmt2 == null && value_result == null) {
-        // nop
-      } else {
-        value_fnclstmt2 = +value_fnclstmt2 || 0
-        value_result = +value_result || 0
-        value_result += value_fnclstmt2
-        result_item[value] = value_result
-        result_items[index] = result_item
+      if( in_item[key] === "1111000") {
+        debugger
       }
-    }
+
+      let index = result_items.findIndex( (result_item) => {
+        return result_item[key] != null && in_item[key] === result_item[key]
+      })
+
+      if(index === -1) {
+        result_items.push(in_item)
+      } else {
+        const result_item = Object.assign( {}, result_items[index] )
+
+        let value_in = in_item[value]
+        let value_result = result_item[value]
+
+        if (value_in == null && value_result == null) {
+          // nop
+        } else {
+          value_in = +value_in || 0
+          value_result = +value_result || 0
+          value_result += value_in
+          result_item[value] = value_result
+          result_items[index] = result_item
+        }
+      }
+    })
   })
 
   consolidated_store.dispatch.bind(consolidated_store)(loadInitData(result_items))
@@ -41,9 +48,6 @@ function recalculate(fnclstmt1_store, fnclstmt2_store, consolidated_store) {
 
 export default function calculation(fnclstmt1_store,
     fnclstmt2_store, consolidated_store) {
-
-  recalculate(fnclstmt1_store, fnclstmt2_store,
-                      consolidated_store)
 
   fnclstmt1_store.subscribe(
     () => {
@@ -58,4 +62,7 @@ export default function calculation(fnclstmt1_store,
                       consolidated_store)
     }
   )
+
+  recalculate(fnclstmt1_store, fnclstmt2_store,
+                      consolidated_store)
 }
